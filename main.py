28d7ba283fcec54516582534ae6b308f8fd8f98f
Tetsuo <Tetsuo.prac2023@student.rugbyschool.ac.th>
2023-02-27 10:29:58 +0700

    Created main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter
+import customtkinter
+
+customtkinter.set_appearance_mode("system")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+app.geometry("800x900")
+
+def button_function():
+    print("button pressed")
+
+# Use CTkButton instead of tkinter Button
+button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
+button.pack()
+
+app.mainloop()
